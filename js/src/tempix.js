701260ebc78ae7baf2b10c14cb1edc5173c8f5e4
Marius Büscher <marius.buescher@gmx.de>
2014-04-25 23:11:22 +0200

    added tempix js

---
+++ js/src/tempix.js
@@ -0,0 +1,68 @@
+(function( window, document, $ ) {
+  'use strict';
+
+  window.Tempix = function () {
+
+    this.templatePrefix = 'tempix-';
+    this.expiration = 10;
+    this.templates = new Object();
+
+    return this;
+  };
+
+  window.Tempix.prototype.downloadTemplate = function (template) {
+    var self = this;
+    $.ajax({
+      url: template.url,
+      context: document.body,
+      async:false,
+      type:"GET",
+      success:function(response) {
+        template.source = response;
+        self.saveTemplate(template);
+      }
+    });
+
+  };
+
+  window.Tempix.prototype.saveTemplate = function (template) {
+    template.tstamp = parseInt(Date.now() / 1000);
+    localStorage.setItem(this.templatePrefix + template.name, JSON.stringify(template));
+  };
+
+  window.Tempix.prototype.get = function () {
+    var result;
+
+    if (arguments.length > 1) {
+      result = new Object();
+
+      for (var i = 0; i < arguments.length; i++) {
+        result[arguments[i]] = this.fetchTemplate(arguments[i]);
+
+        if (result[arguments[i]] === false || (result[arguments[i]].tstamp + this.expiration) < parseInt(Date.now() / 1000) || window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+          this.downloadTemplate(this.templates[arguments[i]]);
+          result[arguments[i]] = this.fetchTemplate(arguments[i]);
+        }
+      }
+
+    } else {
+      result = this.fetchTemplate(arguments[0]);
+
+      if (result === false || (result.tstamp + this.expiration) < parseInt(Date.now() / 1000) || window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+        this.downloadTemplate(this.templates[arguments[0]]);
+        result = this.fetchTemplate(arguments[0]);
+      }
+    }
+
+    return result;
+
+  };
+
+  window.Tempix.prototype.fetchTemplate = function (templateName) {
+    var item = localStorage.getItem(this.templatePrefix+templateName);
+    var template = JSON.parse( item || 'false' );
+
+    return template;
+  };
+
+})( this, document, jQuery );
